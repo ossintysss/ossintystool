05a37c22ebe73c0295e0308e0606472b4de9d988
ossintysss <162944491+ossintysss@users.noreply.github.com>
2024-03-10 20:46:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ def encrypt_pass(password):
     encoded_bytes = base64.b64encode(password.encode())
     print(encoded_bytes)
 
-user_pass = input("Enter your password")
+user_pass = input("Enter your password: ")
 encrypt_pass(user_pass)
+print(f"This is your encrypted password ")
 
